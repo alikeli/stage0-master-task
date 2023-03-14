7bb8516d63fe18c4227ff6a3f5a4b3e94df47a19
alikeli <eli-com2@yandex.ru>
2023-03-14 10:28:07 +0100

    without loops and OOP

--- src/main/java/com/epam/algorithms/ArrayTasks.java
+++ src/main/java/com/epam/algorithms/ArrayTasks.java
@@ -11,7 +11,7 @@ public class ArrayTasks {
      * Return a String[] array that will list all the seasons of the year, starting with winter.
      */
     public String[] seasonsArray() {
-        return null;
+        return new String[]{"winter", "spring", "summer", "autumn"};
     }
 
     /**
@@ -23,7 +23,11 @@ public class ArrayTasks {
      * length = 1  -> [1] length = 3  -> [1, 2, 3] length = 5  -> [1, 2, 3, 4, 5]
      */
     public int[] generateNumbers(int length) {
-        return null;
+        int[] arr = new int[length];
+        for (int i=1; i<=length; i++){
+            arr[i-1] = i;
+        }
+        return arr;
     }
 
     /**
@@ -34,7 +38,11 @@ public class ArrayTasks {
      * arr = [1, 3, 5]   -> sum = 9 arr = [5, -3, -4] -> sum = -2
      */
     public int totalSum(int[] arr) {
-        return 0;
+        int s=0;
+        for (int i : arr) {
+            s = s + i;
+        }
+        return s;
     }
 
     /**
@@ -46,7 +54,12 @@ public class ArrayTasks {
      * arr = [99, -7, 102], number = -7    ->   2 arr = [5, -3, -4],   number = 10    ->  -1
      */
     public int findIndexOfNumber(int[] arr, int number) {
-        return 0;
+        for (int i=0; i<arr.length; i++) {
+            if (arr[i] == number){
+                return i;
+            }
+        }
+        return -1;
     }
 
     /**
@@ -58,7 +71,11 @@ public class ArrayTasks {
      * "pineapple"]
      */
     public String[] reverseArray(String[] arr) {
-        return null;
+        String[] rarr = new String[arr.length];
+        for (int i=0; i<arr.length; i++){
+            rarr[arr.length - i - 1] = arr[i];
+        }
+        return rarr;
     }
 
     /**
@@ -70,7 +87,15 @@ public class ArrayTasks {
      * arr = [1,-2, 3]      -> [1, 3] arr = [-1, -2, -3]   -> [] arr = [1, 2]         -> [1, 2]
      */
     public int[] getOnlyPositiveNumbers(int[] arr) {
-        return null;
+        int s=0, count=0;
+        for (int i: arr){
+            if (i>0) s++;
+        }
+        int[] result = new int[s];
+        for (int i: arr){
+            if (i>0) result[count++]=i;
+        }
+        return result;
     }
 
     /**
@@ -83,7 +108,29 @@ public class ArrayTasks {
      * arr = [[3, 1, 2,], [3,2]] -> [[2, 3], [1, 2, 3]] arr = [[5, 4], [7]]       -> [[7], [4, 5]]
      */
     public int[][] sortRaggedArray(int[][] arr) {
-        return null;
+        for (int i=0; i<arr.length; i++) {
+            for (int j=0; j<arr[i].length; j++){
+                for (int k=j+1; k<arr[i].length; k++){
+                    int tmp;
+                    if (arr[i][j] > arr[i][k]){
+                        tmp = arr[i][j];
+                        arr[i][j] = arr[i][k];
+                        arr[i][k] = tmp;
+                    }
+                }
+            }
+        }
+        for (int i=0; i<arr.length; i++){
+            for (int j=1; j<arr.length-i; j++){
+                int[] tmp;
+                if (arr[j-1].length > arr[j].length){
+                    tmp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = tmp;
+                }
+            }
+        }
+        return arr;
     }
 
 }
--- src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
+++ src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
@@ -3,6 +3,9 @@ package com.epam.langSyntax;
 public class DigitsSumCalculator {
 
     public void calculateSum(int number) {
-    }
 
+        System.out.println(number % 10 + number % 100 / 10 + number % 1000 / 100 + number / 1000);
+    }
 }
+
+
