126f138905a455b4d50bac9bbbbbf00be2c0121c
alikeli <eli-com2@yandex.ru>
2023-03-14 10:32:45 +0100

    done

--- src/main/java/com/epam/OOP/Bird.java
+++ src/main/java/com/epam/OOP/Bird.java
@@ -1,5 +1,15 @@
 package com.epam.OOP;
 
-public class Bird {
+public class Bird extends Animal{
+    public Bird() {
+        super("blue",
+                2 ,
+                false);
+    }
+
+    @Override
+    public String getDescription() {
+        return "This animal is mostly " + color + ". It has " + numberOfPaws + (numberOfPaws > 1 ? " paws" : " paw") + " and " + (hasFur ? "a" : "no") + " fur. Moreover, it has 2 wings and can fly.";
+    }
 
 }
--- src/main/java/com/epam/loops/Pyramid.java
+++ src/main/java/com/epam/loops/Pyramid.java
@@ -4,6 +4,16 @@ public class Pyramid {
 
     public void printPyramid(int cathetusLength) {
 
+        for (int i=0; i<cathetusLength; i++){
+            for (int j=1; j<=cathetusLength + i; j++){
+                if (Math.abs(j - cathetusLength) <= i)
+                    System.out.print(Math.abs(j - cathetusLength) + 1);
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+
     }
 
     public static void main(String[] args) {
